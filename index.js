7a24134dc359bb768df7cdf4bc892528cb8ec196
Omkar Pethe <65779904+har-byte@users.noreply.github.com>
2021-11-08 11:52:28 +0000

    just added some conditions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function reverse(string){
+
+if(!string || string.length<2 || typeof string != 'string' ){
+   console.log("hmmm something went wrong!!!!!!")
+}
+
   var splitstring = string.split("");
   var revstring = splitstring.reverse();
   console.log(revstring);
